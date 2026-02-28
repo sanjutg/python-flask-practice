cc0a691981d37bc34b41d95b018aa693d2f763ac
Sanjeevanaa Ganesh <ganeshsanjeevanaa@gmail.com>
2026-02-28 15:55:39 +0530

    thread example

--- odd-even.py
+++ odd-even.py
@@ -1,14 +1,48 @@
+# import threading
+# import time
+# event_even = threading.Event()
+# event_odd = threading.Event()
+# def even_numbers():
+#     for i in range(0, 10, 2):
+#         print(f"Even: {i}")
+#         event_even.set()
+#         event_odd.wait()
+#         event_odd.clear()
+# def odd_numbers():
+#     for i in range(1, 10, 2):
+#         print(f"Odd: {i}")
+#         event_even.set()
+#         event_odd.wait()
+#         event_odd.clear()
+# def run():
+#     thread_even = threading.Thread(target=even_numbers)
+#     thread_odd = threading.Thread(target=odd_numbers)
+#     thread_even.start()
+#     thread_odd.start()
+#     thread_even.join()
+#     thread_odd.join()
+
 import threading
-import time
+event_even = threading.Event()
+event_odd = threading.Event()
 def even_numbers():
     for i in range(0, 10, 2):
+        event_even.wait()
         print(f"Even: {i}")
-        time.sleep(2)
+        event_even.clear()
+        event_odd.set()
 def odd_numbers():
     for i in range(1, 10, 2):
+        event_odd.wait()
         print(f"Odd: {i}")
-        time.sleep(3)
-even = threading.Thread(target=even_numbers)
-odd = threading.Thread(target=odd_numbers)
-even.start()
-odd.start()
+        event_odd.clear()
+        event_even.set()
+def run():
+    thread_even = threading.Thread(target=even_numbers)
+    thread_odd = threading.Thread(target=odd_numbers)
+    event_even.set()  # Start with even
+    thread_even.start()
+    thread_odd.start()
+    thread_even.join()
+    thread_odd.join()
+run()
